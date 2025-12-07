eb5de905f17aa427e11f6995f8736624633b7115
Tulip Blossom <tulilirockz@outlook.com>
2025-12-07 17:16:44 -0300

    cleanup: remove doc stuff and chsh cuz those are cringe
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -30,6 +30,10 @@ curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub
 rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 systemctl enable flatpak-add-flathub-repos.service
 
+# Saves a ton of space 6767 :steamhappy:
+rm -rf /usr/share/doc
+rm -rf /usr/bin/chsh # footgun
+
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
